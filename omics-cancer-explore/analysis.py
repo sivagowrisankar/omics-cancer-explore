5efceb33b3b4a02afeb13186c554eee8274c06f4
asuragen <siva.gowrisankar@bio-techne.com>
2025-08-16 13:39:43 -0400

    read expr data and dex

---
+++ omics-cancer-explore/analysis.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from scipy.stats import ttest_ind, false_discovery_control
+
+
+def perform_dge(expr_df):
+	"""
+	Performs differential gene expression analysis between matched tumor and normal samples.
+	Returns a DataFrame with DGE results.
+	"""
+	
+	print("Running DGE analysis")
+	# Separate primary tumor (marked by 01 string) and matched normal (marked by 11 string) samples
+	# Ignoring Recurrent and Metastatic Tumors for this exercise
+	tumor_samples = [s for s in expr_df.columns if s.split("-")[3].startswith('01')]
+	normal_samples = [s for s in expr_df.columns if s.split("-")[3].startswith('11')]	
+
+	# Find and sort matched pairs
+	patient_ids = set(s.split('-')[2] for s in tumor_samples) & set(s.split('-')[2] for s in normal_samples)
+	matched_tumor = sorted(s for s in tumor_samples if s.split("-")[2] in patient_ids)
+	matched_normal = sorted(s for s in normal_samples if s.split("-")[2] in patient_ids)
+
+	expr_tumor = expr_df[matched_tumor]
+	expr_normal = expr_df[matched_normal]
+
+	# Perform Welch's t-test for DGE
+	results = []
+	for gene in expr_df.index:
+		t_stat, p_val = ttest_ind(expr_tumor.loc[gene], expr_normal.loc[gene], equal_var=False, nan_policy='omit')
+		log2fc = expr_tumor.loc[gene].mean() - expr_normal.loc[gene].mean()
+		results.append({'gene': gene, 'log2fc': log2fc, 'p_value': p_val})
+
+	dge_results_df = pd.DataFrame(results).dropna().set_index('gene')	
+
+	# Adjust for multiple testing
+	dge_results_df['fdr'] = false_discovery_control(dge_results_df['p_value'])	
+
+	return dge_results_df, matched_tumor, matched_normal
--- omics-cancer-explore/omics-cancer-explore.py
+++ omics-cancer-explore/omics-cancer-explore.py
@@ -1,1 +1,23 @@
+import pandas as pd
+import os
 
+import analysis
+
+def main():
+	"""Main function to run the entire analysis pipeline."""
+	#Ensure output directory exists
+	os.makedirs('output', exist_ok = True)
+
+
+	# Load Data 
+	print("Loading Data...")
+	expr_df = pd.read_csv('../data/TCGA.COADREAD.sampleMap_HiSeqV2.tsv',sep='\t', index_col=0)
+	clinical_df = pd.read_csv('../data/TCGA_MetaData.tsv', sep='\t', index_col=0)
+
+	# Step 1: DGE and Heatmap
+	print("\n--- Running Step 1: DGE and Heatmap--")
+	dge_results, matched_tumor, matched_normal = analysis.perform_dge(expr_df)	
+	print(dge_results)
+
+if __name__ == '__main__':
+	main()
